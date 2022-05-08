474d39aa2b3b650fad968be5a281df453666f2cf
Leonardo Scherer <leoblacena11@gmail.com>
2022-05-08 16:49:12 -0300

    Insert into queries e replies

--- chatbot.sql
+++ chatbot.sql
@@ -7,3 +7,10 @@ CREATE TABLE chat(
     queries VARCHAR(300) NOT NULL,
     replies VARCHAR(300) NOT NULL
 );
+
+INSERT INTO chat (queries, replies) VALUES ('Oi|Olá|Eai|Opa', 'Olá, posso ajudar você?');
+INSERT INTO chat (queries, replies) VALUES ('Bom dia', 'Bom dia, em que posso ajudar?');
+INSERT INTO chat (queries, replies) VALUES ('Boa tarde', 'Boa Tarde, em que posso ajudar?');
+INSERT INTO chat (queries, replies) VALUES ('Boa noite', 'Boa noite, em que posso ajudar?');
+INSERT INTO chat (queries, replies) VALUES ('Quem é você?|Quem tu é|Qual seu nome?|Qual teu nome|', 'Sou o Botzin, estou aqui para ajuda-lo');
+
